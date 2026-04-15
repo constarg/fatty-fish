6bce9591f278eca8111a3733c9be3b96b76e159e
Constantinos Argyriou <argyrioukost@gmail.com>
2026-04-15 22:55:26 +0300

    Add comments.

--- src/main.s
+++ src/main.s
@@ -1,9 +1,32 @@
-.data
+// File: main.s
+//
+// **********************************************************************
+//
+// The entry of the program.  
+//
+// Copyright (C) 2026  Constantinos Argyriou
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Email: constarg@pm.me
+// ***********************************************************************
+.section .rodata
 
 .NO_ARGUMENT_ERR_MSG:
     .string "Not anough arguments provided, argc = %d, required (3).\n"
 
-.text
+.section .text
 
 .extern select_option
 
--- src/options.s
+++ src/options.s
@@ -1,5 +1,28 @@
-
-.data
+// File: options.s
+//
+// **********************************************************************
+//
+// Compares the user provided options against the supported actions, and 
+// executes the coresponding action, if supported. 
+//
+// Copyright (C) 2026  Constantinos Argyriou
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Email: constarg@pm.me
+// ***********************************************************************
+.section .rodata
 
 .help_msg:
     .ascii "Usage: fatty-fish [OPTION_1]... [OPTION_2]\n"
@@ -22,17 +45,50 @@
 .remove_record_opt:
     .string "--remove-record"
 
-.show_record_opt:
+.show_records_opt:
     .string "--show-records"
 
-.text
 
-.equ ST_ARGC, -8
+.section .text
+
+/* Define the relative to %rbp offsets to access
+   the various components stored in the stack 
+   frame. */
+.equ ST_ARGC, -8 
 .equ ST_ARGV, -16
 .equ ST_SIZE, 16
 
+/* The strcmp function used in the procedure, returns
+   zero (0), if the two provided strings are equal. 
+   This constant indicates this case. */
 .equ CMP_EQUALS, 0
 
+/* Define the relative positions of the user provided
+   options inside the ARGV array. */
+.equ ARGV_OPTION_1, 8
+.equ ARGV_OPTION_2, 16
+
+/* Define the function implementations
+   for each of the available options. */
+.extern default_dir_action
+.extern read_record_action
+.extern write_record_action
+.extern remove_record_action
+.extern show_records_action
+
+/**
+ * PURPOSE: This function compares the provided OPTION_1, from the user
+ * against the internally supported options, and executes the coresponding
+ * actions, if the provided option exists, otherwise an error is returned. 
+ *
+ * INPUT: The ARGC and ARGV values. The ARGC should be placed on %rdi and
+ * and address to the first entry of the ARGV should be placed on %rsi. 
+ * 
+ * REGISTERS: This function modifies the value of %rax.
+ *
+ * RETURNS: 0 if the user requiested action completed successfully, otherwise
+ * an negative error is returned, indicating the cause of failure.  
+ */
 .globl select_option
 .type select_option, @function
 select_option:
@@ -47,7 +103,7 @@ select_option:
     # Check which of the available options is requested.
     movq $.default_dir_opt, %rdi 
     movq ST_ARGV(%rbp), %rax
-    movq 8(%rax), %rsi
+    movq ARGV_OPTION_1(%rax), %rsi
     call strcmp
 
 .check_if_deafult_dir_opt:
@@ -81,7 +137,7 @@ select_option:
     jmp .finished_select_option 
 
 .check_if_show_records_opt: 
-    movl $.show_record_opt, %edi
+    movl $.show_records_opt, %edi
     call strcmp 
     cmpl $CMP_EQUALS, %eax
     jne .help_opt 
